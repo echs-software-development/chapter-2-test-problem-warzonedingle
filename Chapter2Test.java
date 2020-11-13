d46b87f8d4c45c123277e3163279da3efc2f97a2
DAVID LEAL <dl0885@students.necsd.net>
2020-11-13 13:15:24 +0000

    I had trouble with it but i think i have it now. Had to dig through notes and repl's to finish it. I had an idea on what to do but had trouble setting up and debugging.

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,25 @@
-
+import java.util.Scanner; 
 
 public class Chapter2Test {
   public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+
+
+  System.out.println("Enter an integer");
+  int num_1 = scan.nextInt();
+
+
+  System.out.println("Enter another interger");
+  int num_2 = scan.nextInt();
+
   
+  System.out.println((double) (num_1 + num_2)/2);
+
+
+  
+
+
+
+
   }
 }
